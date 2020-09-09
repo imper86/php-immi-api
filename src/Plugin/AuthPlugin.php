b8e02de3c64c0b1a5f36e73f39133cea6ab3998c
imper <me@imper.info>
2020-09-09 10:12:43 +0200

    added possibility to add validityInterval. it means you can enter this interval to refresh token when old is not expired yet

--- src/Plugin/AuthPlugin.php
+++ src/Plugin/AuthPlugin.php
@@ -1,9 +1,9 @@
 <?php
 
-
 namespace Imper86\ImmiApi\Plugin;
 
-
+use DateInterval;
+use DateTime;
 use Http\Client\Common\Plugin;
 use Http\Promise\Promise;
 use Imper86\ImmiApi\Enum\GrantType;
@@ -11,6 +11,7 @@ use Imper86\ImmiApi\Model\TokenInterface;
 use Imper86\ImmiApi\Oauth\OauthClientInterface;
 use Imper86\ImmiApi\Oauth\TokenRepositoryInterface;
 use Psr\Http\Message\RequestInterface;
+use RuntimeException;
 
 class AuthPlugin implements Plugin
 {
@@ -22,39 +23,65 @@ class AuthPlugin implements Plugin
      * @var OauthClientInterface
      */
     private $oauthClient;
+    /**
+     * @var DateInterval|null
+     */
+    private $validityInterval;
 
-    public function __construct(TokenRepositoryInterface $tokenRepository, OauthClientInterface $oauthClient)
-    {
+    public function __construct(
+        TokenRepositoryInterface $tokenRepository,
+        OauthClientInterface $oauthClient,
+        ?DateInterval $validityInterval = null
+    ) {
         $this->tokenRepository = $tokenRepository;
         $this->oauthClient = $oauthClient;
+        $this->validityInterval = $validityInterval;
     }
 
-    public function handleRequest(RequestInterface $request, callable $next, callable $first): Promise
-    {
+    public function handleRequest(
+        RequestInterface $request,
+        callable $next,
+        callable $first
+    ): Promise {
         $token = $this->tokenRepository->load();
 
         if (!$token || $request->hasHeader('Authorization')) {
             return $next($request);
         }
 
-        if ($token->isExpired()) {
+        $now = new DateTime();
+
+        if ($this->validityInterval) {
+            $now->add($this->validityInterval);
+        }
+
+        if ($token->isExpired($now)) {
             $token = $this->handleExpired($token);
             $this->tokenRepository->save($token);
         }
 
-        return $next($request->withHeader('Authorization', sprintf('Bearer %s', $token->__toString())));
+        return $next(
+            $request->withHeader(
+                'Authorization',
+                sprintf('Bearer %s', $token->__toString())
+            )
+        );
     }
 
     private function handleExpired(TokenInterface $token): TokenInterface
     {
         if ($token->getRefreshToken()) {
-            return $this->oauthClient->fetchTokenWithRefreshToken($token->getRefreshToken());
+            return $this->oauthClient->fetchTokenWithRefreshToken(
+                $token->getRefreshToken()
+            );
         }
 
         if (GrantType::CLIENT_CREDENTIALS === $token->getGrantType()) {
             return $this->oauthClient->fetchTokenWithClientCredentials();
         }
 
-        throw new \RuntimeException("Can't find a way to refresh expired token");
+        throw new RuntimeException(
+            "Can't find a way to refresh expired token"
+        );
     }
 }
